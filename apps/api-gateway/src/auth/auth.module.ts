635b02c49462c0c4e33fce644fcb695c0cc65eeb
Bhabani Prasad Dash <bhabani.dash@outlook.com>
2025-02-23 18:56:20 +0530

    Implement gRPC & Update for auth microservice (#6)
    * Refactor code to follow module pattern
    
    * Format the code & Fix failing tests
    
    * Implement gRPC & Update for auth microservice

--- apps/api-gateway/src/auth/auth.module.ts
+++ apps/api-gateway/src/auth/auth.module.ts
@@ -2,22 +2,34 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { join } from 'path';
 
 @Module({
   imports: [
     ClientsModule.registerAsync([
       // registerAsyc used to make config module available in this context
       {
-        name: 'AUTH_SERVICE',
+        name: 'AUTH_PACKAGE',
         imports: [ConfigModule], // Ensure ConfigModule is available
         inject: [ConfigService], // Inject ConfigService
-        useFactory: (configService: ConfigService) => ({
-          transport: Transport.TCP,
-          options: {
-            host: configService.get<string>('AUTH_SERVICE_HOST', 'localhost'),
-            port: configService.get<number>('AUTH_SERVICE_PORT', 9001),
-          },
-        }),
+        useFactory: (configService: ConfigService) => {
+          const host = configService.get<string>(
+            'AUTH_SERVICE_HOST',
+            'localhost',
+          );
+          const port = configService.get<number>('AUTH_SERVICE_PORT', 9001);
+          return {
+            transport: Transport.GRPC,
+            options: {
+              package: 'wayfarer.auth',
+              protoPath: join(
+                process.cwd(),
+                'apps/api-gateway/src/proto/auth.proto',
+              ),
+              url: `${host}:${port}`,
+            },
+          };
+        },
       },
     ]),
   ],
