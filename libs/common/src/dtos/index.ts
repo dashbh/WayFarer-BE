a1068bb38b1ebdf1226db9646d41525607c0fb32
Bhabani Prasad Dash <bhabani.dash@outlook.com>
2025-02-25 00:12:11 +0530

    Implement gRPC in catalog Service & Improve Error handling

--- libs/common/src/dtos/index.ts
+++ libs/common/src/dtos/index.ts
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 export * from './auth/register.dto';
 export * from './catalog/catalog.dto';
-=======
-export * from './register.dto';
->>>>>>> 760787622240b0acc326fb049711a7aeb7236600
--- libs/common/src/protos/index.ts
+++ libs/common/src/protos/index.ts
@@ -1,7 +1,4 @@
 import { join } from 'path';
 
 export const AUTH_PROTO_PATH = join(process.cwd(), 'libs/common/src/protos/auth.proto');
-<<<<<<< HEAD
 export const CATALOG_PROTO_PATH = join(process.cwd(), 'libs/common/src/protos/catalog.proto');
-=======
->>>>>>> 760787622240b0acc326fb049711a7aeb7236600
